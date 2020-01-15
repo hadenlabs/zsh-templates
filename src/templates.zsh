13a8e07ed3a8089235d5d939de95a7af772653f9
@slovacus <slovacus@gmail.com>
2020-01-15 07:46:13 -0500

    refactor: change name templates to template (#1)

--- src/templates.zsh
+++ src/templates.zsh
@@ -3,20 +3,20 @@
 
 # shellcheck disable=SC2154  # Unused variables left for readability
 
-function templates::list {
+function template::list {
     # shellcheck disable=SC2002
     cat "${TEMPLATE_SOURCE_PATH}"/data.json | jq '.templates[] | "\(.name) | \(.description) | \(.tool) | \(.type) | \(.filename)"'
 }
 
-function templates::find {
-    local command_value
-    command_value=$(templates::list \
+function template::find {
+    local filename
+    filename=$(template::list \
                     | fzf \
                     | awk '{print $(NF -0)}' \
                     | perl -pe 'chomp' \
                     | sed 's/\"//g'
                 )
-    if [ -n "${command_value}" ]; then
-        echo "${command_value}"
+    if [ -n "${filename}" ]; then
+        echo "${filename}"
     fi
 }
