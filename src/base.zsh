d8f9d7bc61486995bf83091ea7b7e420926a9f71
@slovacus <slovacus@gmail.com>
2020-01-15 07:59:23 -0500

    chore: implement command for read (#1)

--- src/base.zsh
+++ src/base.zsh
@@ -1,7 +1,21 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+# find command for read
+function template::read {
+    local parse_cmd
+    if type -p gtac > /dev/null; then
+        parse_cmd="gtac"
+    elif type -p tac > /dev/null; then
+        parse_cmd="tac"
+    else
+        parse_cmd="tail -r"
+    fi
+    "${parse_cmd}" "${1}"
+}
+
 function fzf::install {
+    # shellcheck disable=SC2154
     message_info "Installing fzf for ${template_package_name}"
     if ! type -p brew > /dev/null; then
         message_warning "Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
