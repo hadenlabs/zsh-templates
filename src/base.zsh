c816ba1cdffef4d5d07440899c218ccd4d8731a3
@slovacus <slovacus@gmail.com>
2020-01-15 07:27:37 -0500

    chore: implement dependence for fzf (#1)

--- src/base.zsh
+++ src/base.zsh
@@ -1,2 +1,14 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
+
+function fzf::install {
+    message_info "Installing fzf for ${template_package_name}"
+    if ! type -p brew > /dev/null; then
+        message_warning "Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+        return
+    fi
+    brew install fzf
+    message_success "Installed fzf for ${template_package_name}"
+}
+
+if ! type -p fzf > /dev/null; then fzf::install; fi
