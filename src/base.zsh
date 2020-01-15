2362189f2974ca34391802f1b9369d48fefdccbc
@slovacus <slovacus@gmail.com>
2020-01-15 09:44:33 -0500

    chore: implement install dependences for most and les (#1)

--- src/base.zsh
+++ src/base.zsh
@@ -1,6 +1,30 @@
 #!/usr/bin/env ksh
 # -*- coding: utf-8 -*-
 
+
+function less::install {
+    message_info "Installing less for ${template_package_name}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${TEMPLATE_MESSAGE_BREW}"
+        return
+    fi
+    brew install less
+    message_success "Installed less for ${template_package_name}"
+
+}
+
+function most::install {
+    message_info "Installing most for ${template_package_name}"
+    if ! type -p brew > /dev/null; then
+        message_warning "${TEMPLATE_MESSAGE_BREW}"
+        return
+    fi
+    brew install most
+    message_success "Installed most for ${template_package_name}"
+
+}
+
+
 # find command for read
 function template::read {
     local parse_cmd
@@ -18,7 +42,7 @@ function fzf::install {
     # shellcheck disable=SC2154
     message_info "Installing fzf for ${template_package_name}"
     if ! type -p brew > /dev/null; then
-        message_warning "Please install brew or use antibody bundle luismayta/zsh-brew branch:develop"
+        message_warning "${TEMPLATE_MESSAGE_BREW}"
         return
     fi
     brew install fzf
@@ -26,3 +50,5 @@ function fzf::install {
 }
 
 if ! type -p fzf > /dev/null; then fzf::install; fi
+if ! type -p most > /dev/null; then most::install; fi
+if ! type -p less > /dev/null; then less::install; fi
