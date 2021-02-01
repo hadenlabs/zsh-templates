5234a7390c8d3d22a17ff037c73ce26b727ace84
@slovacus <slovacus@gmail.com>
2021-02-01 17:20:20 -0500

    refactor: delete load data of json (#10)

--- config/base.zsh
+++ config/base.zsh
@@ -5,7 +5,6 @@ export TEMPLATES_MESSAGE_BREW="Please install brew or use antibody bundle luisma
 export TEMPLATES_MESSAGE_RVM="Please install rvm or use antibody bundle luismayta/zsh-rvm branch:develop"
 
 export TEMPLATES_TEMPLATES_PATH="${ZSH_TEMPLATES_PATH}"/templates
-export TEMPLATES_FILE_TEMPLATES="${TEMPLATES_TEMPLATES_PATH}"/data.json
 
 export TEMPLATES_PACKAGE_NAME=templates
 
